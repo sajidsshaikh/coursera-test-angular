b1a61f74a86b636ce2a440aebc104a536d031ae6
Sajid Shaikh <sajidsha@us.ibm.com>
2016-09-21 12:22:28 -0500

    test

--- app.js
+++ app.js
@@ -2,7 +2,12 @@
 'use strict';
 
 angular.module('myFirstApp', [])
-
+.controller('MySecondController', function ($scope){
+  $scope.lastName = 'Shaikh';
+  $scope.sayHello = function () {
+    return "Hello Coursera!";
+  };
+})
 .controller('MyFirstController', function ($scope) {
   $scope.name = "Yaakov";
   $scope.sayHello = function () {
