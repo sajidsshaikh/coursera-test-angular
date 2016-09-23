ac9794c9d8479ead5fa5e62573a9043b0254db62
Sajid Shaikh <sajidsha@us.ibm.com>
2016-09-23 12:16:35 -0500

    Module1 V1

---
+++ module1-solution/app.js
@@ -0,0 +1,30 @@
+(function () {
+'use strict';
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.message = "";
+  $scope.calculateMenu = function () {
+    var menuVal = $scope.menu;
+    if (menuVal == "") {
+      $scope.message = "Please enter data first";
+    } else {
+      var menuLength = calculateMenuLength(menuVal);
+      if (menuLength <= 3) {
+        $scope.message = "Enjoy!";
+      } else {
+        $scope.message = "Too much!";
+      }
+    }
+
+  };
+}
+
+function calculateMenuLength(menuVal) {
+  var array = menuVal.split(",");
+  console.log("Array Size :" + array.length);
+  return array.length;
+}
+
+})();
