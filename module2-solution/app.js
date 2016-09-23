cc27be680ff85cdbc216eb45534bd2c4f4a4fa60
Sajid Shaikh <sajidsha@us.ibm.com>
2016-09-23 15:23:02 -0500

    Module 2 V1.2

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,31 +1,14 @@
 (function () {
 'use strict';
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope) {
-  $scope.message = "";
-  $scope.calculateMenu = function () {
-    $scope.menu = "";
-    var menuVal = $scope.menu;
-    if (menuVal == "") {
-      $scope.message = "Please enter data first";
-    } else {
-      var menuLength = calculateMenuLength(menuVal);
-      if (menuLength <= 3) {
-        $scope.message = "Enjoy!";
-      } else {
-        $scope.message = "Too much!";
-      }
-    }
-
-  };
+angular.module('ShoppingListCheckOff', [])
+.controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+.controller('ToBuyShoppingController', ToBuyShoppingController);
+AlreadyBoughtShoppingController.$inject = ['$scope'];
+ToBuyShoppingController.$inject = ['$scope'];
+function ToBuyShoppingController($scope) {
 }
 
-function calculateMenuLength(menuVal) {
-  var array = menuVal.split(",");
-  console.log("Array Size :" + array.length);
-  return array.length;
+function AlreadyBoughtShoppingController($scope) {
 }
 
 })();
